f6c2692a1fd000dfc72227f3ae3549b02dec52d0
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-05-22 09:54:33 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+let arreglo1: number[] = new Array(6);
+let arreglo2: number[] = new Array(6);
+let arreglo3: number[] = new Array(6);
+//let suma: number;
+
+  
+let numero1:number, indice1:number
+  for(indice1 = 0; indice1 < 6; indice1++) {
+    numero1 = Number(prompt("Arreglo1: Indique el numero que desea incorporar en la posicion " + indice1));
+    arreglo1[indice1] = numero1;
+  }
+  console.log("Arreglo1");
+  for(indice1 = 0; indice1 < 6; indice1++) {
+    console.log("El numero en la posicion " + indice1 + " es " + arreglo1[indice1]);
+  }
+
+let numero2:number, indice2:number
+  for(indice2 = 0; indice2 < 6; indice2++) {
+    numero2 = Number(prompt("Arreglo2: Indique el numero que desea incorporar en la posicion " + indice2));
+    arreglo2[indice2] = numero2;
+  }
+  console.log("Arreglo2");
+  for(indice2 = 0; indice2 < 6; indice2++) {
+    console.log("El numero en la posicion " + indice2 + " es " + arreglo2[indice2]);
+  };
+ 
+/*function sumarDosArreglos(arreglo1: number, arreglo2: number): number {
+  let i:number;
+  for(i=1; i<=6 ; i++ ) {
+  suma = arreglo1 + arreglo2;
+  };
+    return(suma)
+  }
+  console.log("El arreglo suma es: ", sumarDosArreglos);*/
+
+
+
